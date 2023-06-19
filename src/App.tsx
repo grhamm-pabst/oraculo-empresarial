16d34005987d5f2aa6caca0ff3d990783143f65e
unknown <grhammpabst456@gmail.com>
2023-06-19 16:33:18 -0300

    Começo da interface, adicionando elementos basicos

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,41 @@ function App() {
   }, []);
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <body>
+      <div className='landing-title'>
+        <h1>ORÁCULO EMPRESARIAL</h1>
+      </div>
+      <div className='search-bar'>
+        <div className='prompt'>
+          <div className='option'>
+          Gostaria de saber quem é o responsável
+          </div>
+        </div>
+        <button className='search-button'>
+          DESCOBRIR
+        </button>
+      </div>
+      <div className='containers'>
+        <div className="container" id='container-1'>
+          <h3>1. Primeiro, selecione o início da sua pergunta</h3>
+          <div className='question-box' id='question-1'>
+            dasdasdsa
+          </div>
+        </div>
+        <div className="container" id='container-2'>
+          <h3>2. Agora, especifique o que você quer saber</h3>
+          <div className='question-box' id='question-2'>
+            dasdasdsa
+          </div>
+        </div>
+        <div className="container" id='container-3'>
+          <h3>3. Por fim, conclua a sua pergunta</h3>
+          <div className='question-box' id='question-3'>
+            dasdasdsa
+          </div>
+        </div>
+      </div>
+    </body>
   );
 }
 
